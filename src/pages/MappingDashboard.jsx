f6729031e96e9e65cc2b70d652bb83165e8b7dc4
git <maxime.berthou@ohmsoft.fr>
2025-06-12 18:13:20 +0200

    extraction of bar chart data

--- src/pages/MappingDashboard.jsx
+++ src/pages/MappingDashboard.jsx
@@ -217,6 +217,31 @@ function MappingDashboard() {
     setSelectedParameterValues(parameterValues);
   }, [selectedParameters])
   
+  useEffect(() => {
+    const barChartData = {};
+    for(var [param, values] of selectedParameterValues)
+    {
+      const max = Math.max(...values);
+      const min = Math.min(...values);
+      const step = (max - min) /10;
+      
+      const bining = [];
+      for(let i = 0; i < 10; ++i)
+      {
+        bining.push(
+          {
+            "from" : min + i*step, 
+            "to": min + (i+1)*step,
+            "value" : values.filter(x => x >= min && x < min + step).length
+          });
+      }
+
+      barChartData[param] = bining;
+    }
+
+    console.log(barChartData);
+  }, [selectedParameterValues])
+  
 
   return (
     <div className="px-4 sm:px-6 lg:px-8 py-8 w-full max-w-9xl mx-auto">
